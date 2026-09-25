871c256a542a4b1656a815ff26d20bf4343eeb51
Aosmin <aomsinuki@gmail.com>
2026-09-25 08:05:44 +0700

    Manifest.lua

---
+++ Tabs/Manifest.lua
@@ -0,0 +1,28 @@
+--========================================================
+-- DXPanel Tab Manifest
+--========================================================
+
+return {
+
+    {
+        Name = "Main",
+        Title = "Main",
+        Icon = "◆",
+        Enabled = true,
+    },
+
+    {
+        Name = "Player",
+        Title = "Player",
+        Icon = "●",
+        Enabled = true,
+    },
+
+    {
+        Name = "Settings",
+        Title = "Settings",
+        Icon = "⚙",
+        Enabled = true,
+    },
+
+}
